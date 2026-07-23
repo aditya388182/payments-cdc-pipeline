a2c02e683ba79d4f0e119e0b4d50c5042fe3fabf
aditya388182 <aditgrover17@gmail.com>
2026-07-22 21:49:42 -0500

    aggr fix for 08 after skew

--- scripts/aggregation_skew_job.py
+++ scripts/aggregation_skew_job.py
@@ -34,7 +34,7 @@ def build_spark(naive: bool) -> SparkSession:
     builder = (
         SparkSession.builder.appName("aggregation_skew_job")
         .master("local[2]")
-        .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.1.0")
+        .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.1.0,org.apache.hadoop:hadoop-aws:3.3.4")
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
         .config("spark.sql.catalog.spark_catalog",
                 "org.apache.spark.sql.delta.catalog.DeltaCatalog")
